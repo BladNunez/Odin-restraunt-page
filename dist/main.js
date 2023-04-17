d813acb9a38fc3c2b39467cb80fb81b9dd287d88
BladNunez <Nunez.Bladimir@yahoo.com>
2023-04-17 14:24:01 -0400

    testing webpack new output file

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,17 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+//create div to hold three button tabs
+const contentDiv = document.getElementById('content');
 
+const h1 = document.createElement('h1');
+const d = document.createElement('div');
+
+h1.innerHTML = "It works!";
+d.appendChild(h1);
+
+contentDiv.appendChild(d);
 
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7O0FBRUE7QUFDQTs7QUFFQTtBQUNBOztBQUVBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdHJhdW50LXBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy9jcmVhdGUgZGl2IHRvIGhvbGQgdGhyZWUgYnV0dG9uIHRhYnNcbmNvbnN0IGNvbnRlbnREaXYgPSBkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgnY29udGVudCcpO1xuXG5jb25zdCBoMSA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2gxJyk7XG5jb25zdCBkID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnZGl2Jyk7XG5cbmgxLmlubmVySFRNTCA9IFwiSXQgd29ya3MhXCI7XG5kLmFwcGVuZENoaWxkKGgxKTtcblxuY29udGVudERpdi5hcHBlbmRDaGlsZChkKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
