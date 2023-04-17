f3b809982d29e1d3d8594558de90682085a21a08
BladNunez <Nunez.Bladimir@yahoo.com>
2023-04-17 14:19:38 -0400

    place content on web page to understand webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+//create div to hold three button tabs
+const contentDiv = document.getElementById('content');
 
+const h1 = document.createElement('h1');
+const d = document.createElement('div');
+
+h1.innerHTML = "It works!";
+d.appendChild(h1);
+
+contentDiv.appendChild(d);
